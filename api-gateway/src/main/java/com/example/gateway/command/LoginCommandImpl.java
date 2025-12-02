11af63cf951036fdf8e37b7925d45b7d8f51b6e0
william.halim <william.halim@gdn-commerce.com>
2025-12-02 11:48:44 +0700

    init

---
+++ api-gateway/src/main/java/com/example/gateway/command/LoginCommandImpl.java
@@ -0,0 +1,10 @@
+package com.example.gateway.command;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.security.auth.Login;
+import org.springframework.stereotype.Service;
+
+//@RequiredArgsConstructor
+@Service
+public class LoginCommandImpl  extends LoginCommand {
+}
